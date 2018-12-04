8f9cf722bf0973cbd07965195b0fc2889a539e02
root <chen116@usc.edu>
2018-12-04 07:00:04 -0500

    a

---
+++ latencysimulation-1543920144482/js/all_sessions.js
@@ -0,0 +1,11 @@
+allUsersData = {
+    
+color: '#FF9D00',
+name: 'All Users',
+data: [
+  [1543920147000,1],[1543920148000,1],[1543920149000,1],[1543920150000,1],[1543920151000,1],[1543920152000,1]
+],
+tooltip: { yDecimals: 0, ySuffix: '', valueDecimals: 0 }
+    , zIndex: 20
+    , yAxis: 1
+};
